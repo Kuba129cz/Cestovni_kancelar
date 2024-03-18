165c2f00565da0d2c0af61d74786985836baadd9
Martin Novak <xnovm288@studenti.czu.cz>
2024-03-18 12:31:06 +0100

    aside

---
+++ app/includes/layout/aside.inc.php
@@ -0,0 +1,9 @@
+<aside class="col-3">
+    <ul>
+        <li <?php echo ($title === 'Dashboard') ? 'class="active"' : ''; ?>><a href="#">Link 1</a></li>
+        <li><a href="#" class="btn btn--primary">click</a></li>
+        <li><a href="#" class="btn btn--primary">click</a></li>
+        <li><a href="#" class="btn btn--primary">click</a></li>
+    </ul>            
+    aside (col3)
+</aside>
--- app/page/dashboard.php
+++ app/page/dashboard.php
@@ -5,20 +5,13 @@
     <body>
     <div class="container">
       <div class="row">
-        <aside class="col-3">
-            <ul>
-                <li><a href="#" class="btn btn--primary">click</a></li>
-                <li><a href="#" class="btn btn--primary">click</a></li>
-                <li><a href="#" class="btn btn--primary">click</a></li>
-            </ul>            
-                aside (col3)</aside>
+      <?php include __DIR__.'/../includes/layout/aside.inc.php'; ?>
+
         <main class="col-9">main (col9)</main>
       </div>
     </div>
   </body>
-  <?php
-    include __DIR__.'/../includes/parts/footer.inc.php';
-    ?>
+  <?php include __DIR__.'/../includes/parts/footer.inc.php'; ?>
 </html>
     
     
