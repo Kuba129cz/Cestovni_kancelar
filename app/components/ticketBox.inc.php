bf8a7e7a0764a813fdb7bff115c343f85d0abd40
Martin Novak <xnovm288@studenti.czu.cz>
2024-03-18 12:55:40 +0100

    ticketbox

---
+++ app/components/ticketBox.inc.php
@@ -0,0 +1,5 @@
+<div class="col-4">
+    <div class="ticketBox">
+    Celkem uživatelů: 159
+</div>
+</div>
--- app/page/dashboard.php
+++ app/page/dashboard.php
@@ -7,11 +7,14 @@
       <div class="row">
       <?php include __DIR__.'/../includes/layout/aside.inc.php'; ?>
 
+      <?php include __DIR__.'/../components/ticketBox.inc.php'; ?>
+      
         <main class="col-9">main (col9)</main>
       </div>
     </div>
   </body>
   <?php include __DIR__.'/../includes/parts/footer.inc.php'; ?>
+  <?php include __DIR__.'/../includes/parts/scripts.inc.php'; ?>
 </html>
     
     
