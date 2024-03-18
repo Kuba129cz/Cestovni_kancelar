c2354a3bb0581d3a579e49cb95fbc03e1b4af521
Martin Novak <xnovm288@studenti.czu.cz>
2024-03-18 12:06:11 +0100

    pages

---
+++ app/page/404.php
@@ -0,0 +1,5 @@
+<?php
+    $request=$_SERVER['REQUEST_URI'];//ulozim URL
+    echo "toto neni stranka co hledate";
+    
+?>
--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
     $request=$_SERVER['REQUEST_URI'];//ulozim URL
+    //echo $request;
 
-    echo $request;
+    //redirect
+    switch ($request) {
+        case '' :
+            require __DIR__ . '/app/page/dashboard.php';
+            break;
+        case '/' :
+            require __DIR__ . '/app/page/dashboard.php';
+            break;
+        case '/login' :
+            require __DIR__ . '/app/page/login.php';
+            break;
+        default:
+            http_response_code(404);//aby mě vyhledávače neindexovali neexistujici
+            require __DIR__ . '/app/page/404.php';
+            break;
+    }
+    
 ?>
