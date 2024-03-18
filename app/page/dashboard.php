8184a3d357af6e0db7c39ff7582d1836a5036ae2
Martin Novak <xnovm288@studenti.czu.cz>
2024-03-18 12:18:47 +0100

    componenty

--- app/page/dashboard.php
+++ app/page/dashboard.php
@@ -1,5 +1,24 @@
 <?php
-    $request=$_SERVER['REQUEST_URI'];//ulozim URL
-    echo "home";
-    
+    $title="dashboard";
+    include __DIR__.'/../includes/parts/head.inc.php';
 ?>
+    <body>
+    <div class="container">
+      <div class="row">
+        <aside class="col-3">
+            <ul>
+                <li><a href="#" class="btn btn--primary">click</a></li>
+                <li><a href="#" class="btn btn--primary">click</a></li>
+                <li><a href="#" class="btn btn--primary">click</a></li>
+            </ul>            
+                aside (col3)</aside>
+        <main class="col-9">main (col9)</main>
+      </div>
+    </div>
+  </body>
+  <?php
+    include __DIR__.'/../includes/parts/footer.inc.php';
+    ?>
+</html>
+    
+    
