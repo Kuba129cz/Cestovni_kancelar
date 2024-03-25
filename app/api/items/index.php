e6fdaab7b9a3425a6d0d625c44cb34731ffcc920
Martin Novak <xnovm288@studenti.czu.cz>
2024-03-25 11:59:34 +0100

    bugfix

--- app/api/items/index.php
+++ app/api/items/index.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Methods: GET, POST, DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-require_once __DIR__.'/../controllers/ItemController.php';
+require_once __DIR__ . '/../controllers/ItemController.php';
 
 
 $method=$_SERVER['REQUEST_METHOD'];
@@ -14,5 +14,11 @@ $controller=new ItemController();
 if($method=='GET')
 {
     $items=$controller->getItems();
+    echo json_encode($items);
 }
+else {
+    http_response_code(405); // Method not allowed
+    echo json_encode(['message' => 'Method not allowed.']);
+}
+
 ?>
