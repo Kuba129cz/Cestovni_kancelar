dad97e54ebb7126249035c43ab46bede2d7e93dd
Martin Novak <xnovm288@studenti.czu.cz>
2024-03-25 11:35:31 +0100

    DB conn

--- app/includes/parts/scripts.inc.php
+++ app/includes/parts/scripts.inc.php
@@ -1,1 +1,24 @@
 <script src="//unpkg.com/alpinejs" defer></script>
+
+<script>
+    function adminApp()
+    {
+        return
+        {
+            items: [],//output
+            fetchItems()//zavolej API
+            {
+                fetch('/app/api/items/index.php')
+                .then(Response=>Response.json())
+                .then(data=>
+                {
+                    this.items=data;
+                });
+            },
+            init()
+            {
+                this.fetchItems();
+            }
+        }
+    }
+</script>
