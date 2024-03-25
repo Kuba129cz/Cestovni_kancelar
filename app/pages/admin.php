1e2ad0b35bf901c32b3724db0560ee0585dfb020
Martin Novak <xnovm288@studenti.czu.cz>
2024-03-25 12:35:30 +0100

    vypis

--- app/pages/admin.php
+++ app/pages/admin.php
@@ -11,7 +11,17 @@
         <main class="col-9">
             <div class="row">
               <div x-data="adminApp()">
-                  <template x-if="items.length>0"><div>acb</div></template>
+                  <template x-if="items.length>0">
+                    <template x-for="item in items" :key="item.id">
+                      <div><!--alpine je virtual tak musim pouzit realni DOM-->
+                        <span x-text="item.id"></span>
+                        <span x-text="item.time_stamp"></span>
+                        <span x-text="item.description"></span>
+                        <span x-text="item.destination_id"></span>
+                        <span x-text="item.author_id"></span>
+                    </div>
+                  </template>
+                  </template>
                     
                   <template x-if="items.length===0"><div>null</div></template>
               </div>
