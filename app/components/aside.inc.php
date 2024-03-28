51e30a3001214bd2b630be3d324051bec2968589
Martin Novák <xnovm288@studenti.czu.cz>
2024-03-28 18:34:52 +0100

    prvky side menu

--- app/components/aside.inc.php
+++ app/components/aside.inc.php
@@ -1,9 +1,41 @@
 <aside class="aside col-3" x-bind:class="{ 'aside--close': open }">
     <a href="#" class="btn btn--primary DX-menu" @click="open = !open"></a>
-    <ul>
-        <li <?php echo ($title === 'Dashboard') ? 'class="active"' : ''; ?>><a href="#">Link 1</a></li>
-        <li><a href="#" class="btn btn--primary">click</a></li>
-        <li><a href="#" class="btn btn--primary">click</a></li>
-        <li><a href="#" class="btn btn--primary">click</a></li>
-    </ul>
+    <div>
+        <div>
+            od
+            <input type="date" id="dateFrom" name="trip-start"/>
+        </div>
+        <div>
+            do
+            <input type="date" id="dateTo" name="trip-end"/>
+        </div>
+        <script>
+          document.getElementById('dateFrom').valueAsDate = new Date();
+          document.getElementById('dateTo').valueAsDate = new Date();
+        </script>
+    </div>
+    <div>
+        <div>
+            destinace
+            <select x-model="newItem.destination_id">
+                <option>Vybrat</option>
+                <!--<template x-for="destination in destinations" :key="destination.dest_id">
+                    <option x-bind:value="destination.dest_id" x-text="destination.dest_name"></option>
+                </template>-->
+            </select>
+        </div>
+        <div>
+            typ stravy
+            <select x-model="newItem.destination_id">
+                <option>Vybrat</option>
+                <!--<template x-for="destination in destinations" :key="destination.dest_id">
+                    <option x-bind:value="destination.dest_id" x-text="destination.dest_name"></option>
+                </template>-->
+            </select>
+        </div>
+        <div>
+            cena
+            <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+        </div>
+    </div>
 </aside>
