66f500de26695e6d6faa956c55914492358dc07c
Martin Novák <xnovm288@studenti.czu.cz>
2024-03-29 00:55:39 +0100

    endpoint pro remote stravu

---
+++ app/api/controllers/StravaController.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__.'/../config/remote.php';
+
+class StravaController
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $database=new Database();
+        $this->conn=$database->getConnection();
+    }
+
+    public function getStrava()
+    {
+       $SQL="SELECT * FROM strava";
+       $stmt=$this->conn->prepare($SQL);
+       $stmt->execute();
+       return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+?>
