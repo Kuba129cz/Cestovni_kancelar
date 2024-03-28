54b01a22f77208d8f2f9c43d0d96b773e6a7b556
Martin Novák <xnovm288@studenti.czu.cz>
2024-03-28 20:06:56 +0100

    hiding

--- app/components/aside.inc.php
+++ app/components/aside.inc.php
@@ -2,42 +2,44 @@
     <div>    
         <a href="#" class="btn btn--primary DX-menu" @click="open = !open"></a>
     </div>
-    <div>
-        <div class="sidePair">
-            <span>od</span>
-            <input type="date" id="dateFrom" name="trip-start"/>
+    <div x-bind:class="{ 'sidemenu--close': open }">
+        <div>
+            <div class="sidePair">
+                <span>od</span>
+                <input type="date" id="dateFrom" name="trip-start"/>
+            </div>
+            <div class="sidePair">
+                <span>do</span>
+                <input type="date" id="dateTo" name="trip-end"/>
+            </div>
+            <script>
+              document.getElementById('dateFrom').valueAsDate = new Date();
+              document.getElementById('dateTo').valueAsDate = new Date();
+            </script>
         </div>
-        <div class="sidePair">
-            <span>do</span>
-            <input type="date" id="dateTo" name="trip-end"/>
-        </div>
-        <script>
-          document.getElementById('dateFrom').valueAsDate = new Date();
-          document.getElementById('dateTo').valueAsDate = new Date();
-        </script>
-    </div>
-    <div>
-        <div class="sidePair">
-            <span>destinace</span>
-            <select x-model="newItem.destination_id">
-                <option>Vybrat</option>
-                <!--<template x-for="destination in destinations" :key="destination.dest_id">
-                    <option x-bind:value="destination.dest_id" x-text="destination.dest_name"></option>
-                </template>-->
-            </select>
-        </div>
-        <div class="sidePair">
-            <span>typ stravy</span>
-            <select x-model="newItem.destination_id">
-                <option>Vybrat</option>
-                <!--<template x-for="destination in destinations" :key="destination.dest_id">
-                    <option x-bind:value="destination.dest_id" x-text="destination.dest_name"></option>
-                </template>-->
-            </select>
-        </div>
-        <div class="sidePair">
-            <span>cena</span>
-            <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+        <div>
+            <div class="sidePair">
+                <span>destinace</span>
+                <select x-model="newItem.destination_id">
+                    <option>Vybrat</option>
+                    <!--<template x-for="destination in destinations" :key="destination.dest_id">
+                        <option x-bind:value="destination.dest_id" x-text="destination.dest_name"></option>
+                    </template>-->
+                </select>
+            </div>
+            <div class="sidePair">
+                <span>typ stravy</span>
+                <select x-model="newItem.destination_id">
+                    <option>Vybrat</option>
+                    <!--<template x-for="destination in destinations" :key="destination.dest_id">
+                        <option x-bind:value="destination.dest_id" x-text="destination.dest_name"></option>
+                    </template>-->
+                </select>
+            </div>
+            <div class="sidePair">
+                <span>cena</span>
+                <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+            </div>
         </div>
     </div>
 </aside>
