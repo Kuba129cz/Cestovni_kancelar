8b03151af6c46c5f75b65de4c945b536225c886b
Martin Novák <xnovm288@studenti.czu.cz>
2024-03-29 16:01:25 +0100

    adresa+zajezd

---
+++ app/api/controllers/AdresaController.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__.'/../config/database.php';
+
+class AdresaController
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $database=new Database();
+        $this->conn=$database->getConnection();
+    }
+
+    public function getData()
+    {
+       $SQL="SELECT * FROM Adresa";
+       $stmt=$this->conn->prepare($SQL);
+       $stmt->execute();
+       return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+?>
---
+++ app/api/controllers/ZajezdController.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__.'/../config/database.php';
+
+class ZajezdController
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $database=new Database();
+        $this->conn=$database->getConnection();
+    }
+
+    public function getData()
+    {
+       $SQL="SELECT * FROM zajezd";
+       $stmt=$this->conn->prepare($SQL);
+       $stmt->execute();
+       return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+?>
