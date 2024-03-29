1324c5cec269e30f86506c31c46eab49f9313e23
Martin Novák <xnovm288@studenti.czu.cz>
2024-03-29 15:48:23 +0100

    strava

---
+++ app/api/config/database.remote.php
@@ -0,0 +1,26 @@
+<?php
+
+class Database
+{
+    private $host="cestovka.chgu4kiwyyfo.eu-north-1.rds.amazonaws.com:3306";
+    private $db_name="cestovka";
+    private $username="admin";
+    private $password="12345678";
+
+    private $conn;
+
+    public function getConnection() {
+		$this->conn = null;
+
+		try 
+        {
+			$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+			$this->conn->exec("set names utf8");
+		} catch (PDOException $exception) {
+			echo "Connection error: " . $exception->getMessage();
+		}
+
+		return $this->conn;
+    }
+}
+?>
