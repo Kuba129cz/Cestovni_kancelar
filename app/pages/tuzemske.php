cdbf6449e418b8165ede17507c27bcedb5efeba6
Martin Novák <xnovm288@studenti.czu.cz>
2024-04-04 00:16:46 +0200

    1 file pro vice filtru

--- app/pages/tuzemske.php
+++ app/pages/tuzemske.php
@@ -1,10 +1,20 @@
 <?php
-    $title="tuzemske";
-    include __DIR__.'/../includes/parts/head.inc.php';
+    $title="tuzemske";   
+
+    $kategorie = array(
+      "tuzemske" => array("tuzemske", "'stat=\'Česká republika\''"),
+      "more" => array("more", "'kategorie=\'Moře\''")
+  );
+  $key = isset($_GET['k']) ? $_GET['k'] : null;
+
+  $title= $kategorie[$key][0]; // Outputs: value1
+  $filtr= $kategorie[$key][1]; // Outputs: value2
+  
+  include __DIR__.'/../includes/parts/head.inc.php';
 ?>
     <body x-data="{ open: false }">
     <?php include __DIR__.'/../components/header.inc.php'; ?>
-    <div class="container" x-data="prehledApp('stat=\'Česká republika\'')">
+    <div class="container" x-data="prehledApp(<?php echo"$filtr"?>)">
       <?php include __DIR__.'/../components/aside.inc.php'; ?>
   
       <main class="col-9">
