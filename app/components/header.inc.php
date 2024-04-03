b54b49849c000e5bb9e69ece9b02f93368e9ff8c
Martin Novák <xnovm288@studenti.czu.cz>
2024-04-04 00:55:06 +0200

    buttony

--- app/components/header.inc.php
+++ app/components/header.inc.php
@@ -5,10 +5,11 @@
 </div>
 <header>
     <div class="menu">
-        <a class="menuButton" href="/tuzemske">tuzemské</a>
-        <a class="menuButton" href="index.html">page 1</a>
-        <a class="menuButton" href="index.html">page 3</a>
-        <a class="menuButton" href="index.html">page 4</a>
-        <a class="menuButton" href="index.html">page 5</a>
+        <a class="menuButton" href="/kategorie?k=tuzemske">Tuzemské</a>
+        <a class="menuButton" href="/kategorie?k=hory">Hory</a>
+        <a class="menuButton" href="/kategorie?k=pamatky">Památky</a>
+        <a class="menuButton" href="/kategorie?k=lyze">Lyžování</a>
+        <a class="menuButton" href="/kategorie?k=more">Moře</a>
+        <a class="menuButton" href="/kategorie?k=camp">Camping</a>
     </div>
 </header>
