3e0d6f13b6187021b880b985b6479a80f13cfdbb
Jiri Samsa <xsamj016@studenti.czu.cz>
2024-04-08 19:31:41 +0200

    css dasboard v.1 - Xsamj

--- app/components/zajezd.php
+++ app/components/zajezd.php
@@ -1,21 +1,25 @@
+  
+
   <template x-if="zajezdy.length>0">
       <template x-for="zajezd in zajezdy" :key="zajezd.id_zajezd">
-          <div class="item"><!--alpine je virtual tak musim pouzit realni DOM-->
-              <span x-text="zajezd.id_zajezd"></span>
-              <span x-text="zajezd.datum_prijezdu"></span>
-              <span x-text="zajezd.datum_odjezdu"></span>
-              <span x-text="zajezd.cena_osoba"></span>
-              <!--<span x-text="zajezd.popis"></span>-->
-
-              <span x-text="zajezd.typ_stravy"></span>
-
-              <span x-text="zajezd.stat"></span>
-              <span x-text="zajezd.mesto"></span>
-
-
-          </div>                
+            <div class="grid-container">
+            <div class="item1">Price:<span x-text="zajezd.cena_osoba"></span></div>
+            <div class="item2">Head:<span x-text="zajezd.mesto"></span>/<span x-text="zajezd.stat"></span></div>
+            <div class="item3">From:<span x-text="zajezd.datum_prijezdu"></span></div>  
+            <div class="item4">Order:
+            <a href="/tour">klikni</a>
+            </div>
+            <div class="item5">To:<span x-text="zajezd.datum_odjezdu"></span></div>
+            <div class="item6">Rating:</div>
+            </div> 
       </template>
-</template>
-<template x-if="zajezdy.length===0">
+    
+      </template>
+    <template x-if="zajezdy.length===0">
   <div>null</div>
-</template>
+  </template>
+
+  
+    
+ 
+ 
