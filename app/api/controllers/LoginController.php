10641526b5a2396a8e47824aaf0af6ff1979f256
Martin Novak <xnovm288@studenti.czu.cz>
2024-04-08 12:18:10 +0200

    diagnostika

--- app/api/controllers/LoginController.php
+++ app/api/controllers/LoginController.php
@@ -14,21 +14,25 @@ class LoginController
 
     public function login($nick,$pass)
     {
-       $SQL="select * from User WHERE nick = :nick";
+       //$SQL="select * from User WHERE nick = :nick";
+       $SQL="select * from User WHERE nick = '$nick'";
        if($stmt=$this->conn->prepare($SQL))
        {
-         $stmt->bindParam(":nick",$nick);
+        // $stmt->bindParam(":nick",$nick);
          $stmt->execute();
 
          $result=$stmt->fetchAll(PDO::FETCH_ASSOC);
          if(count($result)>0)
          {
             $user=$result[0];
-            if(password_verify($pass,$user["password"]))
+            //if(password_verify($pass,$user["password"]))
+            if($pass==$user["password"])
             {
                 return['success'=>true, 'username'=>$user['username']];
             }
+            return['success'=>false,'message'=>'blbe heslo'];
          }
+         return['success'=>false,'message'=>'uzivatel nenalezen','SQL'=>$stmt];
          $stmt->closecursor();         
        }
        return['success'=>false];
