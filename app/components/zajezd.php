6f6636fa1c10c34a137a47431e6e58231f924c6e
Jiri Samsa <xsamj016@studenti.czu.cz>
2024-04-08 22:37:31 +0200

    dashboard 3.0 - xsamj016

--- app/components/zajezd.php
+++ app/components/zajezd.php
@@ -3,15 +3,12 @@
   <template x-if="zajezdy.length>0">
       <template x-for="zajezd in zajezdy" :key="zajezd.id_zajezd">
             <div class="grid-container">
-            <div class="item1">Price: <span x-text="zajezd.cena_osoba"></span></div>-
+            <div class="item1">Price: <span x-text="zajezd.cena_osoba"></span></div>
             <div class="item2"><span x-text="zajezd.mesto"></span>/<span x-text="zajezd.stat"></span></div> <!--Název-->
             <div class="item3">From: <span x-text="zajezd.datum_prijezdu"></span></div>  
-            <div class="item4">Order:
-              
-              <a class="btn-order" x-bind:href="'/tour?id=' + zajezd.id_zajezd">Nekup to !</a>
-            </div>
+            <div class="item4"><a class="btn-order" x-bind:href="'/tour?id=' + zajezd.id_zajezd">Nekup to !</a></div>
             <div class="item5">To: <span x-text="zajezd.datum_odjezdu"></span></div>
-            <div class="item6">Rating: </div>
+            <div class="item6">Rating:<span x-text="zajezd.hodnoceni"></span> </div>
             </div> 
       </template>
     
