41e232dbc7e22dfc509e51e5489dcf43e91b761e
Martin Novák <xnovm288@studenti.czu.cz>
2024-04-14 19:09:19 +0200

    admin labels

--- app/components/adminForm.inc.php
+++ app/components/adminForm.inc.php
@@ -1,12 +1,14 @@
 <form @submit.prevent="submitItem">
-    <select x-model="newItem.fk_Adresa">
+    <label for="zajezd_adr">adresa</label>
+    <select id="zajezd_adr" x-model="newItem.fk_Adresa">
         <option>Vybrat</option>
         <template x-for="adresa in adresy" :key="adresa.id_Adresa">
             <option x-bind:value="adresa.id_Adresa" x-text="adresa.stat + ' - ' + adresa.mesto+ ', ' + adresa.ulice"></option>
         </template>
     </select>
 
-    <select x-model="newItem.fk_strava">
+    <label for="zajezd_strava">strava</label>
+    <select id="zajezd_strava" x-model="newItem.fk_strava">
         <option>Vybrat</option>
         <template x-for="strava in stravy" :key="strava.id_strava">
             <option x-bind:value="strava.id_strava" x-text="strava.typ_stravy"></option>
@@ -15,13 +17,16 @@
 
     <textarea x-model="newItem.popis" placeholder="Popis"></textarea>
 
+    <label for="dateFrom">od</label>
     <input type="date" id="dateFrom" x-model="newItem.datum_odjezdu"/>
+    <label for="dateTo">do</label>
     <input type="date" id="dateTo" x-model="newItem.datum_prijezdu"/>
     <script>
         document.getElementById('dateFrom').valueAsDate = new Date();
         document.getElementById('dateTo').valueAsDate = new Date();
     </script>
 
+    <label for="cena_numer">cena na osobu</label>
     <input type="number" id="cena_numer" x-model="newItem.cena_osoba" min="1">
 
     <button type="submit" class="btn btn--primary">Vložit</button>
