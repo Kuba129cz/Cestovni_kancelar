1a2273a8e91b0a5b81fa2cc5f341c3b73d6cf52c
Jiri Samsa <xsamj016@studenti.czu.cz>
2024-04-14 18:47:13 +0200

    adminLayout alfa + fix item - xsamj

--- app/pages/login.php
+++ app/pages/login.php
@@ -4,15 +4,16 @@
       <div class="container" x-data="loginApp()">
 
         <main class="col-9">
-            <form @submit.prevent="login">
+            <form @submit.prevent="login">    
                <div class="flex-container">
+               <div class="zarovnani">
                 <div>Nick:</div>
                 <div><input type="text" x-model="username"/><br></div>
                 <div>Password:</div>
                 <div><input type="password" x-model="password"/><br></div>
                  <div> <button type="submit">login</button><div>
                  </div>
-              
+                </div>
                </form>     
           </main>    
       </div>
