beb6ed78e125181bb6aafdc6a24a25b719f917c3
Martin Novák <xnovm288@studenti.czu.cz>
2024-04-15 22:02:40 +0200

    registr form

--- app/js/loginApp.js.php
+++ app/js/loginApp.js.php
@@ -3,6 +3,7 @@
         return {
             username:'',
             password:'',
+			newZakaznik: { nick:'',password:'',telefon:'',email:'',jmeno: '', prijmeni: '',datum_narozeni: '',fk_Adresa:0},//input submit
             login(){
                 fetch("/app/api/endpoints/login/", {
 						method: 'POST',
@@ -20,6 +21,18 @@
 					})
 					.catch(error => alert(error.message))
 			},
+			registruj() {
+                /*fetch('/app/api/endpoints/objednavka/', {
+						method: 'POST',
+						headers: { 'Content-Type': 'application/json' },
+						body: JSON.stringify(this.newItem)
+					}).then(() => {
+						this.newItem.fk_zajezd = '';
+                        this.newItem.fk_zakaznik = '';
+                        this.newItem.pocet_osob = 0;
+					});*/
+					console.log(this.newZakaznik);
+				},
             init() {//zavola metody
 
             }
