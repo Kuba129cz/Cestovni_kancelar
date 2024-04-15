c093b3fceb18b137e5444f45e9652f9402323898
Martin Novák <xnovm288@studenti.czu.cz>
2024-04-15 23:02:28 +0200

    endpoint teoreticky pripraven, ale nedostanu se pres null check

--- app/api/controllers/ZakaznikController.php
+++ app/api/controllers/ZakaznikController.php
@@ -1,5 +1,7 @@
 <?php
 require_once __DIR__.'/../config/database.php';
+require_once __DIR__.'/UserController.php';
+
 
 class ZakaznikController
 {
@@ -17,6 +19,74 @@ class ZakaznikController
        $stmt=$this->conn->prepare($SQL);
        $stmt->execute();
        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    private function createUser($nick, $password, $telefon, $email)
+    {
+        $query = "INSERT INTO User (nick, password, telefon, email) 
+        VALUES (:nick, :password, :telefon, :email)";
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $nick=htmlspecialchars(strip_tags($nick));
+        $password==htmlspecialchars(strip_tags($password));
+        $telefon=htmlspecialchars(strip_tags($telefon));
+        $email=htmlspecialchars(strip_tags($email));
+
+        $stmt->bindParam(":nick", $nick);
+        $stmt->bindParam(":password", $password);
+        $stmt->bindParam(":telefon", $telefon);
+        $stmt->bindParam(":email", $email);
+
+        if($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+    private function createZakaznik($jmeno, $prijmeni,$datum_narozeni, $fk_Adresa,$fk_user)
+    {
+        $query = "INSERT INTO Zakaznik (jmeno, prijmeni, datum_narozeni, fk_Adresa, fk_user) 
+        VALUES (:jmeno, :prijmeni, :datum_narozeni, :fk_Adresa, :fk_user)";
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $jmeno=htmlspecialchars(strip_tags($jmeno));
+        $prijmeni==htmlspecialchars(strip_tags($prijmeni));
+        $datum_narozeni=htmlspecialchars(strip_tags($datum_narozeni));
+        $fk_Adresa=htmlspecialchars(strip_tags($fk_Adresa));
+
+        $stmt->bindParam(":jmeno", $jmeno);
+        $stmt->bindParam(":prijmeni", $prijmeni);
+        $stmt->bindParam(":datum_narozeni", $datum_narozeni);
+        $stmt->bindParam(":fk_Adresa", $fk_Adresa);
+        $stmt->bindParam(":fk_user", $fk_user);
+
+        if($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function create($nick, $password, $telefon, $email,$jmeno, $prijmeni,$datum_narozeni, $fk_Adresa) 
+    {
+        if (empty($nick) || empty($password) || empty($telefon) || empty($email))
+        {return false;}
+        if(empty($fk_Adresa))
+        {return false;}
+        /*if(empty($jmeno) || empty($prijmeni)|| empty($datum_narozeni)|| empty($fk_Adresa))
+        {return false;}*/
+
+        $user_ctrl=new UserController();
+
+        /*$user_ok=createUser($nick, $password, $telefon, $email);
+        if(!$user_ok){return false;}
+
+        $fk_user=$user_ctrl->getData();
+
+        $zakaznik_ok=createZakaznik($jmeno, $prijmeni,$datum_narozeni, $fk_Adresa, $fk_user);
+        if(!$zakaznik_ok){return false;}*/
+
+        return true;
     }
 }
 ?>
--- app/api/endpoints/Zakaznik/index.php
+++ app/api/endpoints/Zakaznik/index.php
@@ -16,6 +16,17 @@ if($method=='GET')
     $items=$controller->getData();
     echo json_encode($items);
 }
+else if ($method == 'POST') 
+{
+    $data = json_decode(file_get_contents("php://input"), true);
+    if ($controller->create($data['nick'], $data['password'], $data['telefon'],$data['email'], $data['jmeno'], $data['prijmeni'], $data['datum_narozeni'],$data['fk_Adresa'])) {
+        http_response_code(201); // Created
+        echo json_encode(['message' => 'Ticket created successfully.']);
+    } else {
+        http_response_code(503); // Service unavailable
+        echo json_encode(['message' => 'Unable to create ticket.']);
+    }
+}
 else {
     http_response_code(405); // Method not allowed
     echo json_encode(['message' => 'Method not allowed.']);
