bca9d7fca076bca6637414faa4e1d1e0673e32a2
Martin Novák <xnovm288@studenti.czu.cz>
2024-04-19 21:55:06 +0200

    formatování, null-> žádný zájezd

--- app/components/objednavka.php
+++ app/components/objednavka.php
@@ -1,13 +1,15 @@
-  <template x-if="objednavky.length>0">
-      <template x-for="objednavka in objednavky" :key="objednavka.id_objednavka">
-          <div class="item"><!--alpine je virtual tak musim pouzit realni DOM-->
-              <span x-text="objednavka.stat + ' - ' + objednavka.mesto"></span>
-              <span x-text="objednavka.datum_prijezdu"></span>
-              <span x-text="objednavka.datum_odjezdu"></span>
-
-          </div>                
-      </template>
+<template x-if="objednavky.length>0">
+  <template x-for="objednavka in objednavky" :key="objednavka.id_objednavka">
+    <div class="grid-container"><!--alpine je virtual tak musim pouzit realni DOM-->
+      <div class="cena">Price: <span x-text="objednavka.cena_celkem"></span> Kč</div>
+      <div class="nazev"><span x-text="objednavka.mesto"></span>/<span x-text="objednavka.stat"></span></div> <!--Název-->
+      <div class="prijezd">From: <span x-text="objednavka.datum_prijezdu"></span></div>  
+      <div class="objednat"><a class="btn-order" x-bind:href="'/tour?id=' + objednavka.id_zajezd">Nekup to !</a></div>
+      <div class="odjezd">To: <span x-text="objednavka.datum_odjezdu"></span></div>
+      <div class="hodnoceni">Rating:<span x-text="objednavka.hodnoceni"></span> </div>
+    </div>               
+  </template>
 </template>
 <template x-if="objednavky.length===0">
-  <div>null</div>
+  <div>žádná objednávka</div>
 </template>
--- app/components/zajezd.php
+++ app/components/zajezd.php
@@ -1,23 +1,15 @@
-  
-
-  <template x-if="zajezdy.length>0">
-      <template x-for="zajezd in zajezdy" :key="zajezd.id_zajezd">
-            <div class="grid-container">
-            <div class="cena">Price: <span x-text="zajezd.cena_osoba"></span> Kč</div>
-            <div class="nazev"><span x-text="zajezd.mesto"></span>/<span x-text="zajezd.stat"></span></div> <!--Název-->
-            <div class="prijezd">From: <span x-text="zajezd.datum_prijezdu"></span></div>  
-            <div class="objednat"><a class="btn-order" x-bind:href="'/tour?id=' + zajezd.id_zajezd">Nekup to !</a></div>
-            <div class="odjezd">To: <span x-text="zajezd.datum_odjezdu"></span></div>
-            <div class="hodnoceni">Rating:<span x-text="zajezd.hodnoceni"></span> </div>
-            </div> 
-      </template>
-    
-      </template>
-    <template x-if="zajezdy.length===0">
-  <div>null</div>
-  </template>
-
-  
-    
- 
- 
+<template x-if="zajezdy.length>0">
+  <template x-for="zajezd in zajezdy" :key="zajezd.id_zajezd">
+    <div class="grid-container"><!--alpine je virtual tak musim pouzit realni DOM-->
+      <div class="cena">Price: <span x-text="zajezd.cena_osoba"></span> Kč</div>
+      <div class="nazev"><span x-text="zajezd.mesto"></span>/<span x-text="zajezd.stat"></span></div> <!--Název-->
+      <div class="prijezd">From: <span x-text="zajezd.datum_prijezdu"></span></div>  
+      <div class="objednat"><a class="btn-order" x-bind:href="'/tour?id=' + zajezd.id_zajezd">Nekup to !</a></div>
+      <div class="odjezd">To: <span x-text="zajezd.datum_odjezdu"></span></div>
+      <div class="hodnoceni">Rating:<span x-text="zajezd.hodnoceni"></span> </div>
+    </div> 
+  </template>   
+</template>
+<template x-if="zajezdy.length===0">
+<div>žádný zájezd</div>
+</template>
