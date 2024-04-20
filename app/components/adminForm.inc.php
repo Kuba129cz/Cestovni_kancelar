fafadf9a461c17aadece6a4afdb4d409b784197f
Jiri Samsa <xsamj016@studenti.czu.cz>
2024-04-20 20:08:48 +0200

    adminlayout oops - xsamj

--- app/components/adminForm.inc.php
+++ app/components/adminForm.inc.php
@@ -1,33 +1,35 @@
 <form @submit.prevent="submitItem">
-    <label for="zajezd_adr">adresa</label>
-    <select id="zajezd_adr" x-model="newItem.fk_Adresa">
-        <option>Vybrat</option>
-        <template x-for="adresa in adresy" :key="adresa.id_Adresa">
-            <option x-bind:value="adresa.id_Adresa" x-text="adresa.stat + ' - ' + adresa.mesto+ ', ' + adresa.ulice"></option>
+    
+        <label for="zajezd_adr">adresa</label>
+            <select id="zajezd_adr" x-model="newItem.fk_Adresa">
+            <option>Vybrat</option><div>
+            <template x-for="adresa in adresy" :key="adresa.id_Adresa">
+         <option x-bind:value="adresa.id_Adresa" x-text="adresa.stat + ' - ' + adresa.mesto+ ', ' + adresa.ulice"></option>
         </template>
-    </select>
+        </select>
 
-    <label for="zajezd_strava">strava</label>
-    <select id="zajezd_strava" x-model="newItem.fk_strava">
-        <option>Vybrat</option>
-        <template x-for="strava in stravy" :key="strava.id_strava">
-            <option x-bind:value="strava.id_strava" x-text="strava.typ_stravy"></option>
+        <div> <label for="zajezd_strava">strava</label></div>
+        <select id="zajezd_strava" x-model="newItem.fk_strava">
+        <div><option>Vybrat</option></div>
+        <template x-for="strava in stravy" :key="strava.id_strava"></div>
+        <div><option x-bind:value="strava.id_strava" x-text="strava.typ_stravy"></option>
         </template>
-    </select>
+        </select>
+    <div class="flex">
+         <div><textarea x-model="newItem.popis" placeholder="Popis"></textarea></div>
 
-    <textarea x-model="newItem.popis" placeholder="Popis"></textarea>
-
-    <label for="zajezd_dateFrom">od</label>
-    <input type="date" id="zajezd_dateFrom" x-model="newItem.datum_odjezdu"/>
-    <label for="zajezd_dateTo">do</label>
-    <input type="date" id="zajezd_dateTo" x-model="newItem.datum_prijezdu"/>
-    <script>
+        <div><label for="zajezd_dateFrom">od</label></div>
+        <div><input type="date" id="zajezd_dateFrom" x-model="newItem.datum_odjezdu"/></div>
+        <div><label for="zajezd_dateTo">do</label></div>
+        <div><input type="date" id="zajezd_dateTo" x-model="newItem.datum_prijezdu"/></div>
+         <script>
         document.getElementById('zajezd_dateFrom').valueAsDate = new Date();
         document.getElementById('zajezd_dateTo').valueAsDate = new Date();
-    </script>
+        </script>
 
-    <label for="zajezd_cena">cena na osobu</label>
-    <input type="number" id="zajezd_cena" x-model="newItem.cena_osoba" min="1">
+        <div><label for="zajezd_cena">cena na osobu</label></div>
+        <div><input type="number" id="zajezd_cena" x-model="newItem.cena_osoba" min="1"></div>
 
-    <button type="submit" class="btn btn--primary">Vložit</button>
+        <div><button type="submit" class="btn btn--primary">Vložit</button></div>
+    </div>
 </form>
