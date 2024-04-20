d4231d0ddbdbcbc469965dc99eee9520df429163
Jiri Samsa <xsamj016@studenti.czu.cz>
2024-04-20 20:57:22 +0200

    another flex and another - xsamj

--- app/components/adminForm.inc.php
+++ app/components/adminForm.inc.php
@@ -1,5 +1,5 @@
 <form @submit.prevent="submitItem">
-    
+    <div class="flex">
         <label for="zajezd_adr">adresa</label>
             <select id="zajezd_adr" x-model="newItem.fk_Adresa">
             <option>Vybrat</option><div>
@@ -15,21 +15,29 @@
         <div><option x-bind:value="strava.id_strava" x-text="strava.typ_stravy"></option>
         </template>
         </select>
-    <div class="flex">
-         <div><textarea x-model="newItem.popis" placeholder="Popis"></textarea></div>
+    
 
-        <div><label for="zajezd_dateFrom">od</label></div>
-        <div><input type="date" id="zajezd_dateFrom" x-model="newItem.datum_odjezdu"/></div>
-        <div><label for="zajezd_dateTo">do</label></div>
-        <div><input type="date" id="zajezd_dateTo" x-model="newItem.datum_prijezdu"/></div>
-         <script>
-        document.getElementById('zajezd_dateFrom').valueAsDate = new Date();
-        document.getElementById('zajezd_dateTo').valueAsDate = new Date();
-        </script>
 
         <div><label for="zajezd_cena">cena na osobu</label></div>
         <div><input type="number" id="zajezd_cena" x-model="newItem.cena_osoba" min="1"></div>
         </div>
-        <div clas="flex-btn"><button type="submit" class="btn btn--primary">Vložit</button></div>
+
+        <div clas="flex-date">
+            <div><label for="zajezd_dateFrom">od</label></div>
+            <div><input type="date" id="zajezd_dateFrom" x-model="newItem.datum_odjezdu"/></div>
+            <div><label for="zajezd_dateTo">do</label></div>
+            <div><input type="date" id="zajezd_dateTo" x-model="newItem.datum_prijezdu"/></div>
+            <script>
+                document.getElementById('zajezd_dateFrom').valueAsDate = new Date();
+                document.getElementById('zajezd_dateTo').valueAsDate = new Date();
+             </script>
+
+        </div>
+
+
+        <div clas="flex-btn">
+            <div><textarea x-model="newItem.popis" placeholder="Popis" style="width: 850px; height: 88px;"></textarea></div>
+            <div><button type="submit" class="btn btn--primary">Vložit</button></div>
+        </div>
     
 </form>
