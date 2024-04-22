cc11dca6f52f2f605dfd13d85548454f749e4a07
Kuba <k.hampejs@seznam.cz>
2024-04-22 18:29:34 +0200

    xham_contact.php

--- app/pages/contact.php
+++ app/pages/contact.php
@@ -42,7 +42,7 @@
         </div>
     <br><br><br>
 
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12166.652676782505!2d14.364610639102182!3d50.12889284733815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470beaa584b528cd%3A0xf1116b8a274347f7!2sTechnick%C3%A1%20fakulta%20%C4%8CZU%20v%20Praze!5e0!3m2!1scs!2scz!4v1713774630175!5m2!1scs!2scz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe class= "contact-map"src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12166.652676782505!2d14.364610639102182!3d50.12889284733815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470beaa584b528cd%3A0xf1116b8a274347f7!2sTechnick%C3%A1%20fakulta%20%C4%8CZU%20v%20Praze!5e0!3m2!1scs!2scz!4v1713774630175!5m2!1scs!2scz" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </main>
     </div>
 
