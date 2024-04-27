1ba1a56daeff3e1d7bdaf36f46e34610acb77e7f
Kuba <k.hampejs@seznam.cz>
2024-04-27 15:30:57 +0200

    xham_dashboard_responzivita

--- app/pages/dashboard.php
+++ app/pages/dashboard.php
@@ -5,7 +5,7 @@
     <body x-data="{ open: false }">
     <?php include __DIR__.'/../components/header.inc.php'; ?>
     <div class="container" x-data="prehledApp()">
-      <?php include __DIR__.'/../components/aside.inc.php'; ?>
+    <?php include __DIR__.'/../components/aside.inc.php'; ?>
    
       <main class="col-9">
           
